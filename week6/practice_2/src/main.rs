ac73ed807db100335a87e8b392127b0f7b81049f
m.ibrahimCSC101 <mahfuzibrahim9@gmail.com>
2025-11-05 16:25:14 +0100

    (This my second practice of week6)

---
+++ week6/practice_2/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let empty_string = String::new(); 
+    println!("The length of my empty_string is {}", empty_string.len());
+
+    let content_string = String::from("Computerscience"); 
+    println!("The length of content_string is {}",content_string.len());
+    
+}
+
