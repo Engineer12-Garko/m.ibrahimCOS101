5cdada0323d7cbae27ac2b2010c2bc2f38c5c8e6
m.ibrahimCSC101 <mahfuzibrahim9@gmail.com>
2025-11-05 16:54:39 +0100

    (5th practice of week 6)

---
+++ week6/practice_5/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+   let fullname = " Pant-atlantic University";
+   println!();
+   println!("Name: {}",fullname );
+   println!();
+   println!("Before trim ");
+   println!("length is {}", fullname.len());
+   println!();
+   println!("length os {}", fullname.trim().len());
+
+}
