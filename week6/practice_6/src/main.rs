3c6d6d4ac40520569321f95da59dc2914d883a5c
m.ibrahimCSC101 <mahfuzibrahim9@gmail.com>
2025-11-05 18:31:34 +0100

    (6th practice of week 6)

---
+++ week6/practice_6/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let n1 = "Electrical ".to_string();
+    let n2 = "Electronic ".to_string();
+    let n3 = "Engineering ".to_string();
+    let n4 = n1 + &n2 + &n3; // n2 & n3 reference is passed
+
+    // about electrical/electronoc
+    println!("\nThe {} is informed by the aspiration to
+        train electrical/electronic engineering professionals
+        in the areas of the design, building and maintenace of 
+        electrixal control sytem.", n4);
+
+    let w1 = "Computer ".to_string();
+    let w2 = "Science".to_string();
+    let w3 = w1 + &w2; // w2 reference is passed 
+    println!();
+    println!("{} is aimed at developing competent, creative
+        innovative, entrepreneurial and ethically-minded person,
+        capable of creating value in the diverse field of 
+        Computer Science. ", w3);
+
+}
