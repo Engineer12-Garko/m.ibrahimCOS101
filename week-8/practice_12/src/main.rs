200de10d1a4e2453cdd6329f7f57ba78c9c6fdc6
m.ibrahimCSC101 <mahfuzibrahim9@gmail.com>
2025-11-19 22:27:33 +0100

    Creating mutable arrays and changing the index values

---
+++ week-8/practice_12/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    
+    //mutable array 
+    let mut colors = ["red", "green", "yellow", "white"];
+
+    println!("\nOriginal array = {:?}",colors);
+
+    //mutable slice
+    let sliced_colors = &mut colors[1..3];
+
+    println!("First slice = {:?}", sliced_colors);
+
+    //change the value of original slice at thr first index
+    sliced_colors[1] = "purple";
+
+    println!("Changed slice = {:?}", sliced_colors);
+}
